2f9ed7ca3faa8d5e7c053827d5ff9ba6ac296071
Kamran Mehdiyev <kamranmehdiyev@Kamrans-MacBook-Pro.local>
2023-01-16 15:43:12 +0400

    Recipe filtering feature added

--- api/recipe/tests/test_recipe_api.py
+++ api/recipe/tests/test_recipe_api.py
@@ -413,6 +413,46 @@ class PrivateRecipeAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
 
+    def test_filter_by_tags(self):
+        """ Test filtering recipes by tags. """
+        r1 = create_recipe(user=self.user, title='Adana Kebab')
+        r2 = create_recipe(user=self.user, title='Shaurma')
+        tag1 = Tag.objects.create(user=self.user, name='Turkish')
+        tag2 = Tag.objects.create(user=self.user, name='Arabian')
+        r1.tags.add(tag1)
+        r2.tags.add(tag2)
+        r3 = create_recipe(user=self.user, title='Fish and chips')
+
+        params = {'tags': f'{tag1.id},{tag2.id}'}
+        res = self.client.get(RECIPES_URL, params)
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filter_by_ingredients(self):
+        """ Test filtering recipes by ingredients. """
+        r1 = create_recipe(user=self.user, title="Plov")
+        r2 = create_recipe(user=self.user, title='Dovga')
+        ing1 = Ingredient.objects.create(user=self.user, name='Rice')
+        ing2 = Ingredient.objects.create(user=self.user, name='Greenery')
+        r1.ingredients.add(ing1)
+        r2.ingredients.add(ing2)
+        r3 = create_recipe(user=self.user, title='Khangal')
+
+        params = {'ingredients': f'{ing1.id},{ing2.id}'}
+        res = self.client.get(RECIPES_URL, params)
+        s1 = RecipeSerializer(r1)
+        s2 = RecipeSerializer(r2)
+        s3 = RecipeSerializer(r3)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
 
 class ImageUploadTests(TestCase):
     """ Tests for the image upload API. """
