c1fe8b78aef7b9b80eb8e5d57c7a02a650356233
Kamran Mehdiyev <kamranmehdiyev@Kamrans-MacBook-Pro.local>
2023-01-18 20:36:48 +0400

    health check api

---
+++ api/core/views.py
@@ -0,0 +1,11 @@
+"""
+Core views.
+"""
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def health_check(request):
+    """ Returns success message. """
+    return Response({'healthy': True})
